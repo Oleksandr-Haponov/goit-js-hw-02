7b52c2ca523a90ba8cbdba3a985c8fa245e2d2d9
Oleksandr Haponov <alex@skybooking.ua>
2025-03-27 18:22:53 +0200

    Update task-4.js

--- js/task-4.js
+++ js/task-4.js
@@ -1,18 +1,24 @@
 function getShippingCost(country) {
-  let message;
-  const shippingCosts = {
-    China: 100,
-    Chile: 250,
-    Australia: 170,
-    Jamaica: 120,
-  };
-
-  if (shippingCosts[country]) {
-    message = `Shipping to ${country} will cost ${shippingCosts[country]} credits`;
-  } else {
-    message = "Sorry, there is no delivery to your country";
-  }
-  return message;
+    switch (country) {
+        case "Australia":
+            return "Shipping to Australia will cost 170 credits";
+            break
+            case "Germany":
+                return "Sorry, there is no delivery to your country";
+                break
+                case "China":
+                    return "Shipping to China will cost 100 credits";
+                    break
+                    case "Chile":
+                        return "Shipping to Chile will cost 250 credits";
+                        break
+                        case "Jamaica":
+                            return "Shipping to Jamaica will cost 120 credits";
+                            break
+                            default:
+                                return "Sorry, there is no delivery to your country";
+                                break
+}
 }
 
 console.log(getShippingCost("Australia")); // "Shipping to Australia will cost 170 credits"
